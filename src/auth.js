153acb279c7862875c019e81cb508aad92f20a65
zhc7 <hc-zhang22@mails.tsinghua.edu.cn>
2023-10-12 13:54:55 +0800

    feat: add fake register //TMP

--- src/auth.js
+++ src/auth.js
@@ -7,9 +7,16 @@ const url = "http://localhost:8000/"
 const login = (userName, password) => {
     user.value = userName
     console.log("login " + userName)
-    // axios.post(url + "users/login", {userName, password}).then((res) => {
-    //     user.value = res.data
-    // })
+    axios.post(url + "users/login", {userName, password}).then((res) => {
+        user.value = res.data
+    })
 }
 
-export {user, login}
+const register = (userName, password) => {
+    console.log("register " + userName)
+    axios.post(url + "users/register", {userName, password}).then((res) => {
+        user.value = res.data
+    })
+}
+
+export {user, login, register}
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,10 +1,10 @@
 <script setup>
 import {ref} from 'vue';
-import {login} from '../auth.js';
+import {login, register} from '../auth.js';
 import {useRouter} from 'vue-router';
 
 const signupDialog = ref(null);
-const email = ref('');
+const account = ref('');
 const password = ref('');
 
 const router = useRouter();
@@ -13,10 +13,16 @@ const submit = () => {
   login(account.value, password.value);
   router.push("/chat/0")
 }
+
 const dialog = ref(false);
 
+const submitRegister = () => {
+  register(account.value, password.value);
+  dialog.value = false
+}
+
 const showSignUpDialog = () => {
-    signupDialog.value.open();
+  signupDialog.value.open();
 };
 </script>
 
@@ -51,86 +57,87 @@ const showSignUpDialog = () => {
             class="mb-5"
         ></v-text-field>
 
-                <v-row>
-                    <v-col cols="6">
-                        <v-btn color="primary" class="login-btn" @click="submit">登录</v-btn> <!-- 登录按钮 -->
-                    </v-col>
-                    <v-col cols="6">
-                        <div class="flex-grow-1 text-right">
-                            <p>Didn't have an account?</p>
-                            <!-- Sign up dialog -->>
-                            <v-dialog v-model="dialog" persistent width="1024">
-                                <template v-slot:activator="{ props }">
-                                    <a href="#" class="ref-text" v-bind="props">
-                                        Sign up right now!
-                                    </a>
-                                </template>
-                                <v-card>
-                                    <v-card-title>
-                                        <span class="text-h5">User Profile</span>
-                                    </v-card-title>
-                                    <v-card-text>
-                                        <v-container>
-                                            <v-row>
-                                                <v-col cols="12" sm="6" md="4">
-                                                    <v-text-field label="First name*" required></v-text-field>
-                                                </v-col>
-                                                <v-col cols="12" sm="6" md="4">
-                                                    <v-text-field label="Last name*"
-                                                        hint="example of persistent helper text" persistent-hint
-                                                        required></v-text-field>
-                                                </v-col>
-                                                <v-col cols="12">
-                                                    <v-text-field label="Email*" required></v-text-field>
-                                                </v-col>
-                                                <v-col cols="12">
-                                                    <v-text-field label="Password*" type="password" required></v-text-field>
-                                                </v-col>
-                                                <v-col cols="12" sm="6">
-                                                    <v-select :items="['0-17', '18-29', '30-54', '54+']" label="Age*"
-                                                        required></v-select>
-                                                </v-col>
-                                                <v-col cols="12" sm="6">
-                                                    <v-autocomplete
-                                                        :items="['Skiing', 'Ice hockey', 'Soccer', 'Basketball', 'Hockey', 'Reading', 'Writing', 'Coding', 'Basejump']"
-                                                        label="Interests" multiple></v-autocomplete>
-                                                </v-col>
-                                            </v-row>
-                                        </v-container>
-                                        <small>*indicates required field</small>
-                                    </v-card-text>
-                                    <v-card-actions>
-                                        <v-spacer></v-spacer>
-                                        <v-btn color="blue-darken-1" variant="text" @click="dialog = false">
-                                            Close
-                                        </v-btn>
-                                        <v-btn color="blue-darken-1" variant="text" @click="dialog = false">
-                                            Save
-                                        </v-btn>
-                                    </v-card-actions>
-                                </v-card>
-                            </v-dialog>
-                        </div>
-                    </v-col>
-                </v-row>
-
-            </v-col>
+        <v-row>
+          <v-col cols="6">
+            <v-btn color="primary" class="login-btn" @click="submit">登录</v-btn> <!-- 登录按钮 -->
+          </v-col>
+          <v-col cols="6">
+            <div class="flex-grow-1 text-right">
+              <p>Didn't have an account?</p>
+              <!-- Sign up dialog -->>
+              <v-dialog v-model="dialog" persistent width="1024">
+                <template v-slot:activator="{ props }">
+                  <a href="#" class="ref-text" v-bind="props">
+                    Sign up right now!
+                  </a>
+                </template>
+                <v-card>
+                  <v-card-title>
+                    <span class="text-h5">User Profile</span>
+                  </v-card-title>
+                  <v-card-text>
+                    <v-container>
+                      <v-row>
+                        <v-col cols="12" sm="6" md="4">
+                          <v-text-field label="First name*" required></v-text-field>
+                        </v-col>
+                        <v-col cols="12" sm="6" md="4">
+                          <v-text-field label="Last name*"
+                                        hint="example of persistent helper text" persistent-hint
+                                        required></v-text-field>
+                        </v-col>
+                        <v-col cols="12">
+                          <v-text-field label="Account*" v-model="account"  required></v-text-field>
+                        </v-col>
+                        <v-col cols="12">
+                          <v-text-field label="Password*" v-model="password" type="password" required></v-text-field>
+                        </v-col>
+                        <v-col cols="12" sm="6">
+                          <v-select :items="['0-17', '18-29', '30-54', '54+']" label="Age*"
+                                    required></v-select>
+                        </v-col>
+                        <v-col cols="12" sm="6">
+                          <v-autocomplete
+                              :items="['Skiing', 'Ice hockey', 'Soccer', 'Basketball', 'Hockey', 'Reading', 'Writing', 'Coding', 'Basejump']"
+                              label="Interests" multiple></v-autocomplete>
+                        </v-col>
+                      </v-row>
+                    </v-container>
+                    <small>*indicates required field</small>
+                  </v-card-text>
+                  <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn color="blue-darken-1" variant="text" @click="dialog = false">
+                      Close
+                    </v-btn>
+                    <v-btn color="blue-darken-1" variant="text" @click="submitRegister">
+                      Save
+                    </v-btn>
+                  </v-card-actions>
+                </v-card>
+              </v-dialog>
+            </div>
+          </v-col>
         </v-row>
-    </v-container>
 
-    <SignUp ref="signupDialog" />
+      </v-col>
+    </v-row>
+  </v-container>
+
+  <SignUp ref="signupDialog"/>
 </template>
 
 
 <style>
 .login-btn {
-    width: 88px;
-    height: 49.5px !important;
-    margin-right: 40px;
-    align-items: center;
+  width: 88px;
+  height: 49.5px !important;
+  margin-right: 40px;
+  align-items: center;
 }
+
 .ref-text {
-    color: primary;
-    cursor: pointer;
+  color: primary;
+  cursor: pointer;
 }
 </style>
