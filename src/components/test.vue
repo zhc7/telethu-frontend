94c900b7920e96714759033848b39986ebd4b756
申君皓 <2022010726@secoder.net>
2023-10-12 21:02:30 +0800

    feat: added test.vue.

---
+++ src/components/test.vue
@@ -0,0 +1,9 @@
+<template>
+  <v-container fluid>
+    <v-row class="fill-height">
+      <v-col :cols="3">Left Part</v-col>
+      <v-col :cols="6">Middle Part</v-col>
+      <v-col :cols="3">Right Part</v-col>
+    </v-row>
+  </v-container>
+</template>
