9d1d07501b194abea918fdb0e98b17977e2bfbca
zhc7 <hc-zhang22@mails.tsinghua.edu.cn>
2023-10-12 13:13:01 +0800

    fix: route

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,8 @@
-<script setup>
-import Login from './views/Login.vue';
-import MessageFlow from "./components/MessageFlow.vue";
-</script>
+<script setup></script>
 
 <template>
-  <MessageFlow room_id="0"/>
+  <router-view />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
 </style>
