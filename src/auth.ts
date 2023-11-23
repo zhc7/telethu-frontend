0861f4cf3eed1ed1ec559bb56990a3c47d3ee86d
zhc7 <hc-zhang22@mails.tsinghua.edu.cn>
2023-11-23 14:37:12 +0800

    fix(auth): do not delete users data when logging out

--- src/auth.ts
+++ src/auth.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import {BASE_API_URL, DEBUG} from "./constants"
 import {useLocalStorage} from "@vueuse/core"
-import {users, user} from "./globals";
+import {user} from "./globals";
 
 const token = useLocalStorage("token", "");
 
@@ -34,7 +34,6 @@ const login = async (email: string, password: string) => {
 
 const logout = () => {
     token.value = "";
-    users.value = {};
     user.value = {email: "", id: -1, name: "", avatar: "", category: "user"};
 }
 
