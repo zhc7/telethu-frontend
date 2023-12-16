4036d495eef88d22af586f27422d16029c85a0c7
zhc7 <hc-zhang22@mails.tsinghua.edu.cn>
2023-12-16 14:58:31 +0800

    feat: enable https connection

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,8 @@
 export const DEBUG = true;
-export const TEST_SERVER = false;
+const TEST_SERVER = false;
+const HTTPS = true;
 const BASE_URL = TEST_SERVER ? "localhost:8000" : "telethu-backend-secoder-E8A.app.secoder.net";
-export const BASE_API_URL = 'http://' + BASE_URL + '/';
-export const BASE_WS_URL = "ws://" + BASE_URL + "/";
+export const BASE_API_URL = `http${HTTPS ? 's' : ''}://` + BASE_URL + '/';
+export const BASE_WS_URL = `ws${HTTPS ? 's' : ''}://` + BASE_URL + "/";
 
 export const LANGUAGE = "zh-cn";
