b9d0525b63948c24d3aa03cfeed33e934f51108e
wyifei26 <yifei-wa22@mails.tsinghua.edu.cn>
2023-12-18 17:20:16 +0800

    fix: do not default test server

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 export const DEBUG = true;
-const TEST_SERVER = true;
-const HTTPS = false;
+const TEST_SERVER = false;
+const HTTPS = true;
 const BASE_URL = TEST_SERVER ? "localhost:8000" : "telethu-backend-secoder-E8A.app.secoder.net";
 export const BASE_API_URL = `http${HTTPS ? 's' : ''}://` + BASE_URL + '/';
 export const BASE_WS_URL = `ws${HTTPS ? 's' : ''}://` + BASE_URL + "/";
