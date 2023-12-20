acc3ac3238bbed80bacba6d79de59e73e1c191db
wyifei26 <yifei-wa22@mails.tsinghua.edu.cn>
2023-12-20 21:02:28 +0800

    feat: prettify group search msg

--- src/components/GroupSearchMessage.vue
+++ src/components/GroupSearchMessage.vue
@@ -4,11 +4,12 @@ import {computed, ref} from "vue";
 import {getUser} from "../core/data.ts";
 import {Message} from "../utils/structs.ts";
 import axios from "axios";
-import {BASE_API_URL} from "../constants.ts";
+import {BASE_API_URL, DEBUG} from "../constants.ts";
 import {token} from "../auth.ts";
 import {activeMessageId} from "../globals.ts";
 import MessagePopItem from "./MessagePopItem.vue";
 import List from "./List.vue";
+import {callSnackbar} from "../utils/snackbar.ts";
 
 
 const props = defineProps<{
@@ -38,7 +39,7 @@ const searchFromBack = async () => {
     id: props.groupId,
   };
   if (fromTime.value) {
-    params.from =  +new Date(fromTime.value);
+    params.from = +new Date(fromTime.value);
   }
   if (toTime.value) {
     params.to = +new Date(toTime.value);
@@ -57,9 +58,11 @@ const searchFromBack = async () => {
     headers: {
       Authorization: token.value,
     }
-  });
-  resultMessages.value = result.data;
-  console.log(result);
+  }).then((res) => {
+    resultMessages.value = res.data;
+    callSnackbar(resultMessages.value.length + ' messages found', 'success')
+  }).catch((err) => callSnackbar(err.response.data.message, 'error'));
+  if (DEBUG) console.log(result);
 }
 
 const resultMessages = ref<Message []>([]);
@@ -73,39 +76,42 @@ const zero = ref(0);
       <v-card-title class="text-h5 text-center my-3 ma-4">
         Search for Message in Group {{ getUser(groupId).name }}
       </v-card-title>
-      <v-card-text class="overflow-y-auto d-flex flex-column ml-10 mr-10">
-        <v-row class="my-2">
-          <v-col cols="12">
-            <v-text-field
-                v-model="fromTime"
-                label="From"
-                type="datetime-local"
-                variant="underlined"
-            ></v-text-field>
-          </v-col>
-          <v-col cols="12">
-            <v-text-field
-                v-model="toTime"
-                label="To"
-                type="datetime-local"
-                variant="underlined"
-            ></v-text-field>
-          </v-col>
-        </v-row>
-        <v-select
-            :items="messageTypes"
-            label="Select Message Type"
-            v-model="selectedMessageType"
-            class="my-2"
-            variant="outlined"
-        ></v-select>
-        <v-text-field
-            class="my-2"
-            label="Message Content"
-            v-model="messageContentInput"
-            variant="outlined"
-        />
-        <v-btn color="primary" class="mt-2" @click="searchFromBack">Search</v-btn>
+      <v-card-text class="overflow-y-auto d-flex flex-column">
+        <div style="margin-left: 5vw; margin-right: 5vw">
+              <v-text-field
+                  v-model="fromTime"
+                  label="From"
+                  type="datetime-local"
+                  variant="underlined"
+                  density="compact"
+              ></v-text-field>
+              <v-text-field
+                  v-model="toTime"
+                  label="To"
+                  type="datetime-local"
+                  variant="underlined"
+                  density="compact"
+              ></v-text-field>
+          <v-select
+              :items="messageTypes"
+              label="Select Message Type"
+              v-model="selectedMessageType"
+              class="my-2"
+              variant="outlined"
+              density="compact"
+          ></v-select>
+          <v-text-field
+              class="my-2"
+              label="Message Content"
+              v-model="messageContentInput"
+              variant="outlined"
+              density="compact"
+          />
+        </div>
+        <div class="button-container">
+          <v-btn color="primary" @click="searchFromBack" variant="tonal">Search</v-btn>
+        </div>
+        <v-divider class="my-2" v-if="resultMessages.length"></v-divider>
         <List v-if="resultMessages.length" class="overflow-y-auto" min-height="15vh" v-model="activeMessageId">
           <MessagePopItem
               :jump="true"
@@ -117,7 +123,7 @@ const zero = ref(0);
       </v-card-text>
       <v-card-actions class="mb-3 mr-4">
         <v-spacer/>
-        <v-btn color="grey darken-1" @click="dialog=false">
+        <v-btn color="grey darken-1" @click="dialog=false; resultMessages=[]">
           Close
         </v-btn>
       </v-card-actions>
@@ -135,5 +141,10 @@ label {
   margin-bottom: 5px;
   font-weight: bold;
   color: #555;
+}
+
+.button-container {
+  display: flex;
+  justify-content: center;
 }
 </style>
